4ce0110d3a71a8eecc3dd79f759a70b6b935fa34
Bruno Peres <bruno_16515@hotmail.com>
2023-08-22 11:44:00 -0300

    :tada: Initial commit

---
+++ src/app/task/page.tsx
@@ -0,0 +1,13 @@
+export default function TasksPage() {
+  return (
+    <div className="flex flex-col items-center pt-20">
+      <h1 className="text-2xl font-bold text-neutral-50 mb-16">CRUD de Tarefas Next.JS</h1>
+
+      <main className="flex flex-col gap-4 max-w-2xl w-full">
+        <div className="w-full px-8 py-6 bg-neutral-900 border-2 border-neutral-700 rounded-2xl">
+
+        </div>
+      </main>
+    </div>
+  )
+}
